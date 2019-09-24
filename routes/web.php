3f66780ed9f1e05070c62ebcc8460462836c0cfa
Faye Donohoe <N00174001@iadt.ie>
2019-09-24 14:13:25 +0100

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+# @Date:   2019-09-17T15:43:10+01:00
+# @Last modified time: 2019-09-24T13:56:15+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/fayedonohoe', function () {
+    return view('faye');
+});
